115f66cf33cc21a21e06364d80bac52d7da1eaf2
Alex <dovzhanyn.alex@gmail.com>
2016-11-26 13:16:11 -0500

    some changes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,21 @@
 class UserController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user!, only: :profile
 
 	def show
+		@user = User.find(params[:id])
+	end
+
+	def list
+		@users = User.all
+	end
+
+	def find_and_redirect
+		user = User.find(params[:id])
+
+		redirect_to "/users/#{user.id}/#{user.username}"
+	end
+
+	def profile
 		render 'profile'
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,22 @@ Rails.application.routes.draw do
   
 	get '/' => 'application#index'
 
-	get '/profile' => 'user#show'
+	get '/profile' => 'user#profile'
 
+	get '/snippets' => 'application#index'
 	get '/snippets/new' => 'snippet#create'
 	get '/snippets/:id' => 'snippet#find_and_redirect'
 	get '/snippets/:id/:slug' => 'snippet#show'
+
+	get '/users' => 'user#list'
+	get '/users/:id' => 'user#find_and_redirect'
+	get '/users/:id/:slug' => 'user#show'
 
 	post '/post_snippet' => 'snippet#post_snippet'
 
 	post '/refactor/create' => 'refactor#create'
 	post '/refactors/:id/upvote' => 'refactor#upvote'
+
 
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, :controllers => { registrations: 'registrations' }
   
